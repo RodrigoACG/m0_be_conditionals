096637ce0aa5397199c062304ccf1eaa9d859ced
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-01-12 16:27:46 -0700

    Mild challenge complete.

--- code_challenges/mild.rb
+++ code_challenges/mild.rb
@@ -1,2 +1,9 @@
 # Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
+
+number = 2
+if number.even? 
+    p "even"
+else
+    p "odd"
+end
